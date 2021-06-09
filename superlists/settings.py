670512516bc023b532fd28747c3e8602629c998e
abbasegbeyemi <abbasegbeyemi@gmail.com>
2021-06-09 09:16:45 +0100

    Forced DEBUG to true for testing

--- superlists/settings.py
+++ superlists/settings.py
@@ -18,15 +18,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-DEBUG = True
 if "DJANGO_DEBUG_FALSE" in os.environ:
-    # DEBUG = False
+    DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     ALLOWED_HOSTS = os.environ["SITENAME"]
 else:
-    # DEBUG = True
+    DEBUG = True
     SECRET_KEY = "insecure-key-for-dev"
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ["SITENAME"]
 
 # Application definition
 
