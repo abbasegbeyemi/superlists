e720823e039971289174e411141d512f92710b71
abbasegbeyemi <abbasegbeyemi@gmail.com>
2021-06-09 09:51:06 +0100

    Changed settings for allowed hosts.

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-    ALLOWED_HOSTS = os.environ["SITENAME"]
+    ALLOWED_HOSTS = os.environ["SITENAME"].split(",")
 else:
     DEBUG = True
     SECRET_KEY = "insecure-key-for-dev"
-    ALLOWED_HOSTS = os.environ["SITENAME"].split(",")
+    ALLOWED_HOSTS = []
 
 # Application definition
 
