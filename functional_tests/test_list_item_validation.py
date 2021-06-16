39b18c45830018d5026068118936d227051e6351
abbasegbeyemi <abbasegbeyemi@gmail.com>
2021-06-16 08:38:17 +0100

    Refactored functional tests to use multiple files

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,11 +7,15 @@ class ItemValidationTest(FunctionalTest):
         # She hits enter on an empty listbox
 
         # Home page refreshes and there is an error message sayig the input box cannot be blank.
-
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.selenium.find_element_by_css_selector(".has-error").text,
+                "You can't have an empty list item")
+        )
         # She tries again with some text which now works.
 
         # She then tries again with another empty imput box and gets a similar error
 
         # She can correct it by filling some text in
 
-        self.fail("Write me!")
+        # self.fail("Write me!")
